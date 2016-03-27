b8d0d49b670595ed2a5daeb35896ed622e9a5430
Stiven Deleur <deleurapps@gmail.com>
2016-03-27 14:17:38 -0700

    added board init and boardFromFEN

--- src/board.py
+++ src/board.py
@@ -3,3 +3,33 @@ Created on Mar 22, 2016
 
 @author: Stiven
 '''
+
+
+setMask = []
+clearMask = []
+kingAttacks = []
+knightAttacks = []
+
+RANK_1 = 0x00000000000000FF
+RANK_8 = 0xFF00000000000000
+FILE_A = 0x0101010101010101
+FILE_H = 0x8080808080808080
+LEFT_HALF = 0xF0F0F0F0F0F0F0F0
+
+WHITE, BLACK, PAWNS, KNIGHTS, BISHOPS, ROOKS, QUEENS, KINGS = 0, 1, 2, 3, 4, 5, 6, 7
+
+WKCA, WQCA, BKCA, BQCA = 1, 2, 4, 8
+
+class Side:
+    W, B, NONE, BOTH = range(4)
+
+class Board:
+    def __init__(self):
+        self.pieceBitBoards = []
+        self.castleRights = 0
+        self.WCastled = False
+        self.BCastled = False
+        self.EPTarget = None
+        self.halfMoveClock = 0
+        self.fullMoveCounter = 0
+        self.sideToMove = Side.NONE
--- src/util.py
+++ src/util.py
@@ -3,3 +3,78 @@ Created on Mar 22, 2016
 
 @author: Stiven
 '''
+import board
+from board import Board
+from board import Side
+
+def boardFromFEN(FEN):
+    newBoard = Board()
+    subFEN = FEN.split(" ")
+    piecesByRow = subFEN[0].split("/")
+    sideToMove = subFEN[1]
+    castlingRights = subFEN[2]
+    enPassantTarget = subFEN[3]
+    halfMoveClock = int(subFEN[4])
+    fullMoveCounter = int(subFEN[5])
+
+    y = 7
+    for row in piecesByRow:
+        x = 0
+
+        for piece in piecesByRow:
+            if piece.isdigit():
+                x += int(piece)
+            else:
+                pieceIsUpperCase = piece != piece.lower()
+                piece = piece.lower()
+                if piece == 'p':
+                    newBoard.pieceBitBoards[board.PAWNS] |= coordToBit((x, y))
+                elif piece == 'n':
+                    newBoard.pieceBitBoards[board.KNIGHTS] |= coordToBit((x, y))
+                elif piece == 'b':
+                    newBoard.pieceBitBoards[board.BISHOPS] |= coordToBit((x, y))
+                elif piece == 'r':
+                    newBoard.pieceBitBoards[board.ROOKS] |= coordToBit((x, y))
+                elif piece == 'k':
+                    newBoard.pieceBitBoards[board.KINGS] |= coordToBit((x, y))
+                elif piece == 'q':
+                    newBoard.pieceBitBoards[board.QUEENS] |= coordToBit((x, y))
+                if pieceIsUpperCase:
+                    newBoard.pieceBitBoards[board.WHITE] |= coordToBit((x, y))
+                else:
+                    newBoard.pieceBitBoards[board.BLACK] |= coordToBit((x, y))
+                x += 1
+        y -= 1
+
+    newBoard.sideToMove = Side.W if sideToMove == "w" else Side.B
+    for c in castlingRights:
+        if c == 'K':
+            castlingRights |= board.WKCA
+        elif c == "Q":
+            castlingRights |= board.WQCA
+        elif c == "k":
+            castlingRights |= board.BKCA
+        elif c == "q":
+            castlingRights |= board.BQCA
+    newBoard.EPTarget = stringToCoord(enPassantTarget)
+    newBoard.halfMoveClock = halfMoveClock
+    newBoard.fullMoveCounter = fullMoveCounter
+    return newBoard
+
+def indexToBit(index):
+    bit = 1 << index
+    return bit
+
+def coordToIndex(coord):
+    index = coord[1]*8+coord[0]
+    return index
+
+def coordToBit(coord):
+    return indexToBit(coordToIndex(coord))
+
+def stringToCoord(loc):
+    if len(loc) != 2:
+        return None
+    x = ord(loc[0].upper()) - 65
+    y = int(loc[1]) - 1
+    return (x, y)
